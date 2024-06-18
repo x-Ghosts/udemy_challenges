69566d1f2f1e15150eb689c1a3a449a62506556f
x-Ghosts <tnewnis2@gmail.com>
2024-06-18 02:14:08 +0100

    Day 7 - Final Project of the Day

--- Day_7/Day7_Project_Hangman.py
+++ Day_7/Day7_Project_Hangman.py
@@ -10,6 +10,7 @@ from stages import stages, logo
 
 word_list = words_list.words
 display = []
+previous_guesses = [] #this to track previous user guesses
 
 # Random word choice from the machine.
 chosen_word = random.choice(word_list)
@@ -22,17 +23,26 @@ for letter in chosen_word:
 
 # Check the identity match of the letter chosen within the word randomly picked
 def game_process(lifes):
-    for list_index in range(0, len(chosen_word)):
-        if chosen_word[list_index] == guess:
-            display.insert(list_index, guess)
-            display.pop(list_index + 1)
-            print(stages[lifes] + "\n")
-    if guess not in chosen_word:
+    if guess in previous_guesses and guess in chosen_word:
+        print(f"You already chose the letter '{guess}' previously.\n")
+    elif guess in previous_guesses and guess not in chosen_word:
         lifes = lifes - 1
         print(stages[lifes] + "\n")
-        print(f"Oops ! The chosen letter '{guess} is not fitting in there ! \nLife Cost : 1. \nRemaining life: {lifes}.\n")
-    if guess in chosen_word:
-        print(f"You already chose the letter '{guess}' previously.\n")
+        print(f"You already chose the letter '{guess}' previously.\nWrong Choice ! Liffe Cost : 1. \nRemaining life: {lifes}.\n")
+    else:
+        previous_guesses.append(guess)
+        if guess not in chosen_word:
+            lifes = lifes - 1
+            print(stages[lifes] + "\n")
+            print(f"Oops ! The chosen letter '{guess} is not fitting in there ! \nLife Cost : 1. \nRemaining life: {lifes}.\n")
+        else:
+            for list_index in range(0, len(chosen_word)):
+                if chosen_word[list_index] == guess:
+                    display.insert(list_index, guess)
+                    display.pop(list_index + 1)
+            
+            print(stages[lifes] + "\n")
+                    
 
     print(display)
     return lifes
