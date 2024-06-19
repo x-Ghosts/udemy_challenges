ff8c386c7ce36de5a23d5111face6a93f1c259b0
x-Ghosts <tnewnis2@gmail.com>
2024-06-19 20:04:22 +0100

    Day 8 - Final Project of the Day

--- Day_8/Day_8_Final_Project.py
+++ Day_8/Day_8_Final_Project.py
@@ -19,7 +19,7 @@ shift = int(input("Type the shift number: \n"))
 
 
 
-def encrypt(text,shift):
+'''def encrypt(text,shift):
     count = 0
     encoded_message = ''
     while not count == shift:
@@ -58,12 +58,47 @@ def decrypt(text, shift):
 
     print(decoded_output)
     decoded_message = decoded_message.join(decoded_output)
-    print(f"The decoded message is {str(decoded_message)}.")
-        
-        
+    print(f"The decoded message is {str(decoded_message)}.")'''
+
+def caeser(direction, text, shift):
+    count = 0
+    encoded_message = ''
+    decoded_message = ''
+    if direction.lower() == 'encode':
+        while not count == shift:
+            for i in range (0, shift,1):
+                count += 1
+                letter = shifted_alphabet.pop(0)
+                shifted_alphabet.append(letter)
+        for letters in text:
+            if letters in alphabet:
+                index = alphabet.index(letters)
+                encoded_letter = shifted_alphabet[index]
+                encoded_output.append(encoded_letter)
+            else:
+                index = text.index(letters)
+                encoded_output.insert(index, letters)
+    
+        encoded_message = encoded_message.join(encoded_output)
+        print(f"The encoded message is {str(encoded_message)}.")
+
+    elif direction.lower() == 'decode':
+        while not count == shift:
+            for i in range (0, shift):
+                count += 1
+                letter = shifted_alphabet.pop()
+                shifted_alphabet.insert(0,letter)
+        for letters in text:
+            if letters in shifted_alphabet:
+                index = alphabet.index(letters)
+                decoded_letter = shifted_alphabet[index]
+                decoded_output.append(decoded_letter)
+            else:
+                index = text.index(letters)
+                decoded_output.insert(index, letters)
+
+        decoded_message = decoded_message.join(decoded_output)
+        print(f"The decoded message is {str(decoded_message)}.")
 
 
-if direction.lower() == 'encode':
-    encrypt(text, shift)
-elif direction.lower() == 'decode':
-    decrypt(text,shift)
+caeser(direction, text, shift)
