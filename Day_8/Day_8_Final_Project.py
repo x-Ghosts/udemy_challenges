97287f3756044afb7e792bd5a7a5f736c28130b4
x-Ghosts <tnewnis2@gmail.com>
2024-06-19 19:53:12 +0100

    Day 8 - Final Project of the Day

--- Day_8/Day_8_Final_Project.py
+++ Day_8/Day_8_Final_Project.py
@@ -6,13 +6,14 @@ from logo import logo
 print(logo)
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 shifted_alphabet = alphabet.copy()
 encoded_output = []
+decoded_output = []
 
 
 
-direction = input("Type 'encode' to encrypt, or decode to 'decrypt': \n")
+direction = input("Type 'encode' to encrypt, or 'decode' to decrypt: \n")
 text = input("Write the message: \n").lower()
 shift = int(input("Type the shift number: \n"))
 
@@ -38,8 +39,31 @@ def encrypt(text,shift):
     encoded_message = encoded_message.join(encoded_output)
     print(f"The encoded message is {str(encoded_message)}.")
 
+def decrypt(text, shift):
+    count = 0
+    decoded_message = ''
+    while not count == shift:
+        for i in range (0, shift):
+            count += 1
+            letter = shifted_alphabet.pop()
+            shifted_alphabet.insert(0,letter)
+    for letters in text:
+        if letters in shifted_alphabet:
+            index = alphabet.index(letters)
+            decoded_letter = shifted_alphabet[index]
+            decoded_output.append(decoded_letter)
+        else:
+            index = text.index(letters)
+            decoded_output.insert(index, letters)
+
+    print(decoded_output)
+    decoded_message = decoded_message.join(decoded_output)
+    print(f"The decoded message is {str(decoded_message)}.")
+        
         
 
 
 if direction.lower() == 'encode':
     encrypt(text, shift)
+elif direction.lower() == 'decode':
+    decrypt(text,shift)
