75a73aa670b605a9c47b5d090b62ac43574ccbbc
x-Ghosts <tnewnis2@gmail.com>
2024-06-19 20:26:41 +0100

    Day 8 - Final Project of the Day

--- Day_8/Day_8_Final_Project.py
+++ Day_8/Day_8_Final_Project.py
@@ -10,6 +10,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 shifted_alphabet = alphabet.copy()
 encoded_output = []
 decoded_output = []
+stop_program = False
 
 
 
@@ -18,47 +19,6 @@ text = input("Write the message: \n").lower()
 shift = int(input("Type the shift number: \n"))
 
 
-
-'''def encrypt(text,shift):
-    count = 0
-    encoded_message = ''
-    while not count == shift:
-        for i in range (0, shift,1):
-            count += 1
-            letter = shifted_alphabet.pop(0)
-            shifted_alphabet.append(letter)
-    for letters in text:
-        if letters in alphabet:
-            index = alphabet.index(letters)
-            encoded_letter = shifted_alphabet[index]
-            encoded_output.append(encoded_letter)
-        else:
-            index = text.index(letters)
-            encoded_output.insert(index, letters)
-    
-    encoded_message = encoded_message.join(encoded_output)
-    print(f"The encoded message is {str(encoded_message)}.")
-
-def decrypt(text, shift):
-    count = 0
-    decoded_message = ''
-    while not count == shift:
-        for i in range (0, shift):
-            count += 1
-            letter = shifted_alphabet.pop()
-            shifted_alphabet.insert(0,letter)
-    for letters in text:
-        if letters in shifted_alphabet:
-            index = alphabet.index(letters)
-            decoded_letter = shifted_alphabet[index]
-            decoded_output.append(decoded_letter)
-        else:
-            index = text.index(letters)
-            decoded_output.insert(index, letters)
-
-    print(decoded_output)
-    decoded_message = decoded_message.join(decoded_output)
-    print(f"The decoded message is {str(decoded_message)}.")'''
 
 def caeser(direction, text, shift):
     count = 0
@@ -86,7 +46,7 @@ def caeser(direction, text, shift):
         while not count == shift:
             for i in range (0, shift):
                 count += 1
-                letter = shifted_alphabet.pop()
+                letter = shifted_alphabet.pop() 
                 shifted_alphabet.insert(0,letter)
         for letters in text:
             if letters in shifted_alphabet:
@@ -102,3 +62,16 @@ def caeser(direction, text, shift):
 
 
 caeser(direction, text, shift)
+
+while stop_program == False:
+
+    user_input = input("Type 'yes' if you want to go again. Otherwise, type 'no'.")
+    if user_input.lower() == 'yes':
+        direction = input("Type 'encode' to encrypt, or 'decode' to decrypt: \n")
+        text = input("Write the message: \n").lower()
+        shift = int(input("Type the shift number: \n"))
+        caeser(direction, text, shift)
+        stop_program = False
+    else:
+        print("Goodbye")
+        stop_program = True
