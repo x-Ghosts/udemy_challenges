7d3ebb0b537f6bcb913b5d1cdc10b8f40c61c4e1
x-Ghosts <tnewnis2@gmail.com>
2024-06-19 17:17:53 +0100

    Day 8 - Final Project of the Day

--- Day_8/Day_8_Final_Project.py
+++ Day_8/Day_8_Final_Project.py
@@ -1,6 +1,10 @@
 # Main objective is to create a program that encode and decode a message using ceaser cypher, according to the video instructions;
 # Link of the video is : https://www.udemy.com/course/100-days-of-code/learn/lecture/19211072#overview (Course enrolling is mandatory)
 
+from logo import logo
+
+print(logo)
+
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 shifted_alphabet = alphabet.copy()
