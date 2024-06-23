1a6ee9fa13708e2d463317149b2f96874396ffad
x-Ghosts <tnewnis2@gmail.com>
2024-06-23 18:06:55 +0100

    Day 9 - Final Project of the Day

--- Day_9/Day9_Final_Project.py
+++ Day_9/Day9_Final_Project.py
@@ -1,9 +1,12 @@
 # Main objective is to create a program that does a blind auction, according to the video instructions;
 # Link of the video is : https://www.udemy.com/course/100-days-of-code/learn/lecture/19279394#overview (Course enrolling is mandatory)
 
-
+from ascii_art import logo
+from os import system
 
 auction_list = []
+
+print(logo + "\n")
 name = input("What's your name ? ")
 bid = float(input("What's your bid ? $"))
 stop_program = False
@@ -22,17 +25,19 @@ bid_input(name, bid)
 print(auction_list)
 
 while stop_program == False:
-    multiple_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
+    multiple_bidders = input("Are there any other bidders? Type 'yes' or 'no'. \n")
     if multiple_bidders.lower() == 'yes':
+        system('cls') #this for clearing the page, because I'm coding directly via VSCode
         name = input("What's your name ? ")
         bid = float(input("What's your bid ? $"))
         bid_input(name, bid)
         print(auction_list)
     elif multiple_bidders.lower() == 'no':
+        system('cls')
         auction_max_value = 0
         for i in range (0, len(auction_list)):
             if auction_list[i]['bid'] > auction_max_value:
                 index_max = i
                 auction_max_value = auction_list[i]['bid']
-        print(f"The winner is '{auction_list[index_max]['name']}' with ${auction_max_value}.")
+        print(f"The winner is {auction_list[index_max]['name']} with ${auction_max_value}.")
         stop_program = True
